0cb83640ffa929c03bc10c694b27a603fe874591
Evgenia Tikhomirova <gerrlin88@gmail.com>
2025-03-24 22:25:59 +0300

    1. Базовая сортировка по 3 полям;
    2. Создан бинарный поиск

--- src/main/java/com/finalproject/model/Animal.java
+++ src/main/java/com/finalproject/model/Animal.java
@@ -1,11 +1,12 @@
 package com.finalproject.model;
+import java.util.Comparator;
 
 public class Animal {
     private final String species;
     private final String eyeColor;
     private final boolean hasFur;
 
-    private Animal(AnimalBuilder animalBuilder) {
+    private Animal(com.finalproject.model.Animal.AnimalBuilder animalBuilder) {
         this.species = animalBuilder.species;
         this.eyeColor = animalBuilder.eyeColor;
         this.hasFur = animalBuilder.hasFur;
@@ -16,24 +17,30 @@ public class Animal {
         private String eyeColor;
         private boolean hasFur;
 
-        public AnimalBuilder setSpecies(String species) {
+        public com.finalproject.model.Animal.AnimalBuilder setSpecies(String species) {
             this.species = species;
             return this;
         }
 
-        public AnimalBuilder setEyeColor(String eyeColor) {
+        public com.finalproject.model.Animal.AnimalBuilder setEyeColor(String eyeColor) {
             this.eyeColor = eyeColor;
             return this;
         }
 
-        public AnimalBuilder setHasFur(boolean hasFur) {
+        public com.finalproject.model.Animal.AnimalBuilder setHasFur(boolean hasFur) {
             this.hasFur = hasFur;
             return this;
         }
 
-        public Animal build() {
-            return new Animal(this);
+        public com.finalproject.model.Animal build() {
+            return new com.finalproject.model.Animal(this);
         }
+    }
+
+    public static Comparator<com.finalproject.model.Animal> getComparator() {
+        return Comparator.comparing((com.finalproject.model.Animal a) -> a.species)
+                .thenComparing(a -> a.eyeColor)
+                .thenComparing(a -> a.hasFur);
     }
 
     @Override
