f16c1a3381b0826b364f1cff20e6c5d1a513b645
HIHIHAHA <kruker.tester@gmail.com>
2025-03-26 01:29:26 +0300

    Добавлен класс для рандомной генерации объектов

---
+++ src/main/java/com/finalproject/creator/RandomObjectCreator.java
@@ -0,0 +1,68 @@
+package com.finalproject.creator;
+
+import com.finalproject.model.Animal;
+import com.finalproject.model.Barrel;
+import com.finalproject.model.Human;
+import java.util.Random;
+
+public class RandomObjectCreator {
+
+    private static final String[] ANIMAL_SPECIES = {"Кот", "Собака", "Лев", "Тигр", "Обезьяна"};
+    private static final String[] ANIMAL_EYE_COLORS = {"Синий", "Зеленый", "Желтый", "Карий"};
+    private static final String[] BARREL_STORED_MATERIALS = {"Вода", "Пиво", "Мед", "Масло"};
+    private static final String[] BARREL_MATERIALS = {"Дуб", "Сталь", "Пластик"};
+    private static final String[] HUMAN_GENDER = {"Мужской", "Женский"};
+    private static final String[] HUMAN_SURNAMES = {"Сидорович", "Brown", "Hopkins", "Godfrey", "Smith"};
+
+    private final Random random;
+
+    public RandomObjectCreator() {
+        this.random = new Random();
+    }
+
+    // Генерация случайного животного
+    public Animal createRandomAnimal() {
+        return new Animal.AnimalBuilder()
+                .setSpecies(ANIMAL_SPECIES[random.nextInt(ANIMAL_SPECIES.length)])
+                .setEyeColor(ANIMAL_EYE_COLORS[random.nextInt(ANIMAL_EYE_COLORS.length)])
+                .setHasFur(random.nextBoolean())
+                .build();
+    }
+
+    // Генерация случайной бочки
+    public Barrel createRandomBarrel() {
+        return new Barrel.BarrelBuilder()
+                .setVolume(random.nextInt(200) + 1) // Объем от 1 до 200 литров
+                .setStoredMaterial(BARREL_STORED_MATERIALS[random.nextInt(BARREL_STORED_MATERIALS.length)])
+                .setMaterial(BARREL_MATERIALS[random.nextInt(BARREL_MATERIALS.length)])
+                .build();
+    }
+
+    // Генерация случайного человека
+    public Human createRandomHuman() {
+        return new Human.HumanBuilder()
+                .setGender(HUMAN_GENDER[random.nextInt(HUMAN_GENDER.length)])
+                .setAge(random.nextInt(80) + 1) // Возраст от 1 до 80 лет
+                .setSurname(HUMAN_SURNAMES[random.nextInt(HUMAN_SURNAMES.length)])
+                .build();
+    }
+
+    @Override
+    public String toString() {
+        return "RandomObjectCreator{" +
+                "random=" + random +
+                '}';
+    }
+
+    // Генерация случайного объекта любого типа
+    public Object createRandomObject() {
+        int choice = random.nextInt(3); // Выбираем между Animal (0), Barrel (1), Human (2)
+        return switch (choice) {
+            case 0 -> createRandomAnimal();
+            case 1 -> createRandomBarrel();
+            default -> createRandomHuman();
+        };
+
+
+    }
+}
